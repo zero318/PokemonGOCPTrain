49d661b2b26d164d41e5587b0d47a2eb2859e2cc
zero318 <zero318hackingcorp@gmail.com>
2020-01-30 15:36:06 -0500

    Started adding the ability to operate from the command line and cleaned up a bunch of excess files

--- PokemonGOCPTrain/CPCalculator.c
+++ PokemonGOCPTrain/CPCalculator.c
@@ -16,20 +16,42 @@
 #define MaxCPM 44
 #define CPMCount ((MaxCPM - MinCPM) + 1)
 
-//#define PrintCrap
+#define DoSomething(OtherMacro) do {OtherMacro} while (0)
+#define DoNothing do {} while (0)
 
-#define DoWrap(OtherMacro) \
-do {\
-	OtherMacro\
-} while (0)
+#define VerboseMode Verbose
+#define VerboseDigits VerboseLength
+#define DenominatorName VerboseDenominator
+#define NumeratorName VerboseNumerator
 
 //This macro is literally just a toggleable printf.
 //The syntax is the same as the regular printf, but
 //it can be toggled on/off by defining PrintCrap
+#define PrintCrap
+
 #ifdef PrintCrap
-#define CrappyPrintf(...) DoWrap(printf(__VA_ARGS__);)
+#define CrappyPrintf(...) DoSomething(printf(__VA_ARGS__);)
+#define VerboseMessage(Label) DoSomething(\
+if (VerboseMode) {\
+	printf(Label);\
+})
+#define VerboseProgressHeader(Label, Length) DoSomething(\
+if (VerboseMode) {\
+	printf(Label);\
+	VerboseDigits = (char)(floor(log10(Length) + 1) + 48); \
+	DenominatorName[1] = VerboseDigits + 1; \
+	NumeratorName[2] = VerboseDigits; \
+	printf(DenominatorName, '/', Length);\
+})
+#define VerboseProgress(Value) DoSomething(\
+if (VerboseMode) {\
+	printf(NumeratorName, Value);\
+})
 #else
-#define CrappyPrintf(...) do {} while (0)
+#define CrappyPrintf(...) DoNothing
+#define VerboseMessage(Label) DoNothing
+#define VerboseProgressHeader(Label, Length) DoNothing
+#define VerboseProgress(Value) DoNothing
 #endif
 
 //These macros are essentially just malloc/calloc with
@@ -81,7 +103,6 @@ typedef struct PokemonStatsStruct {
 } PokemonStatsStruct;
 
 typedef struct CPComboStruct {
-	//uint32_t HPIV : 4, DefenseIV : 4, AttackIV : 4, Level : 7, Form : 3, Dex : 10;
 	uint_least32_t HPIV : 5, DefenseIV : 5, AttackIV : 5, Level : 7, Index : 10;
 } CPComboStruct;
 #pragma pack(pop)
@@ -100,28 +121,77 @@ inline void* SaferResourceLoad(int ResourceFile) {
 	return LockResource(GenericResource);
 }
 
+//inline char* VerbosePrintfProgressDenominator(void* Number) {
+//	double Intermediate = *(double*)Number;
+//	char VerboseBuilder[6] = { '%', (char)(floor(log10(Intermediate) + 1)), 'c', '/', '%', 'u' };
+//	return VerboseBuilder;
+//}
+//
+//inline char* VerbosePrintfProgressNumerator(void* Number) {
+//	double Intermediate = *(double*)Number;
+//	char VerboseBuilder[4] = { '\r', '%', (char)(floor(log10(Intermediate) + 1)), 'u'};
+//	return VerboseBuilder;
+//}
+
 int main(int argc, char* argv[]) {
 	//Set the process priority as high as possible since maybe it'll run faster
 	HANDLE CalculatorProcess = GetCurrentProcess();
 	SetPriorityClass(CalculatorProcess, REALTIME_PRIORITY_CLASS);
 	//uint_fast8_t MinLevel = 0, MaxLevel = 88;
-	/*if (argc > 1) {
-		uint_fast8_t LastCrapWasFlag;
+	uint_fast8_t OutputMode = 0;
+	//These are defined as macros so they can be updated gloablly
+	uint_fast8_t VerboseMode = 0;
+	uint_fast8_t VerboseDigits;
+	char DenominatorName[6] = { '%', '\0', 'c', '%', 'u', '\0' };
+	char NumeratorName[5] = { '\r', '%', '\0', 'u', '\0' };
+	uint_fast16_t SpecificCP = 0;
+	if (argc > 1) {
+		/*uint_fast8_t LastCrapWasFlag;
 		for (int i = 1; i < argc; ++i) {
 			
-		}
+		}*/
+		CrappyPrintf("Pokemon GO CP Combination Calculator\n");
 	}
 	else {
-		MinLevel = 0;
-		MaxLevel = 88;
-	}*/
-	CrappyPrintf("Pokemon GO CP Combination Calculator\nLoading CPMs\n%u\n", LevelCount);
+		printf("Pokemon GO CP Combination Calculator\n"
+			   "1. XLSX Mode (Verbose)\n"
+			   "2. XLSX Mode\n"
+			   "3. Text Mode (Verbose)\n"
+			   "4. Text Mode\n"
+		       "5. Exit\n"
+		       "Select a mode: ");
+		uint_fast8_t UserIsDumb = 0;
+		switch (getchar()) {
+			case '1':
+				Verbose = 1;
+			case '2':
+				OutputMode = 0;
+				break;
+			case '3':
+				Verbose = 1;
+			case '4':
+				OutputMode = 1;
+				printf("Select a CP value: ");
+				do {
+					UserIsDumb = scanf("%u", &SpecificCP);
+				} while (!UserIsDumb);
+				break;
+			case '5':
+				printf("Yee");
+				exit(EXIT_SUCCESS);
+			default:
+				printf("That wasn't an option. :P");
+				exit(EXIT_SUCCESS);
+		}
+	}
 	double* CPM = SaferMalloc(double, CPM, LevelCount);
 	float* CPMFloats = (float*)SaferResourceLoad(PokemonCPMFile);
-	//float* CPMFloats = (float*)LockResource(LoadResource(NULL, FindResource(NULL, MAKEINTRESOURCE(PokemonCPMFile), RT_RCDATA)));
 	uint_fast8_t CPMIndex = 0;;
+	//CrappyPrintf("Loading CPMs:\n  /%2u\r", LevelCount - 1);
+	VerboseProgressHeader("Loading CPMs:\n", LevelCount - 1);
 	for (uint_fast8_t Level = MinLevel; Level <= MaxLevel; ++Level) {
-		CrappyPrintf("%u\r", Level);
+		//CrappyPrintf("%2u\r", Level);
+		VerboseProgress(Level);
 		if (Level & 1) {
 #pragma warning(suppress:26451)
 			CPM[Level] = sqrt(CPMFloats[CPMIndex] * CPMFloats[CPMIndex] - CPMFloats[CPMIndex] * CPMFloats[CPMIndex] / 2 + CPMFloats[CPMIndex + 1] * CPMFloats[CPMIndex + 1] / 2);
@@ -138,16 +208,16 @@ int main(int argc, char* argv[]) {
 	uint_fast32_t CPIndex = 0;
 	//This struct is used for holding many temporary values
 	PokemonStatsStruct* PokemonStats = (PokemonStatsStruct*)SaferResourceLoad(PokemonStats2File);
-	//PokemonStatsStruct* PokemonStats = (PokemonStatsStruct*)LockResource(LoadResource(NULL, FindResource(NULL, MAKEINTRESOURCE(PokemonStats2File), RT_RCDATA)));
-	//PokemonStatsStruct PokemonStats2;
 	FastCPValue CP;
 	uint_fast16_t AttackStats[IVCount];
 	double DefenseStats[IVCount], HPStats[IVCount];
 	double AttackDefense;
-	CrappyPrintf("\nPokemon Count Pass 1:\n%u\n", PokemonCount);
+	//CrappyPrintf("\nPokemon Count Pass 1:\n   /%3u\r", PokemonCount - 1);
+	VerboseProgressHeader("\nPokemon Count Pass 1:\n", PokemonCount - 1);
 	uint_fast16_t Pokemon;
 	for (Pokemon = MinPokemon; Pokemon <= MaxPokemon; ++Pokemon) {
-		CrappyPrintf("%u\r", Pokemon);
+		//CrappyPrintf("%3u\r", Pokemon);
+		VerboseProgress(Pokemon);
 		PokemonStatsStruct PokemonStats2 = PokemonStats[Pokemon];
 		for (uint_fast8_t IV = MinIV; IV <= MaxIV; ++IV) {
 			AttackStats[IV] = PokemonStats2.BaseAttack + IV;
@@ -200,11 +270,13 @@ int main(int argc, char* argv[]) {
 			CPColumnHeight[CP] = 0;
 		}
 	}
-	CrappyPrintf("\nPokemon Count Pass 2:\n%u\n", PokemonCount);
+	//CrappyPrintf("\nPokemon Count Pass 2:\n   /%3u\r", PokemonCount - 1);
+	VerboseProgressHeader("\nPokemon Count Pass 2:\n", PokemonCount - 1);
 	CPIndex = 0;
 	CPComboStruct Combo;
 	for (Combo.Index = MinPokemon; Combo.Index <= MaxPokemon; ++Combo.Index) {
-		CrappyPrintf("%u\r", Combo.Index);
+		//CrappyPrintf("%3u\r", Combo.Index);
+		VerboseProgress(Combo.Index);
 		for (Combo.Level = MinLevel; Combo.Level <= MaxLevel; ++Combo.Level) {
 			for (Combo.AttackIV = MinIV; Combo.AttackIV <= MaxIV; ++Combo.AttackIV) {
 				for (Combo.DefenseIV = MinIV; Combo.DefenseIV <= MaxIV; ++Combo.DefenseIV) {
@@ -224,77 +296,109 @@ int main(int argc, char* argv[]) {
 	//Load a file with a bunch of strings representing Excel's column headers
 	//Because screw trying to actually calcuate those dang letters on the fly
 	//All indices are multiplied by 4 to account for max string length
-	char* EndMySuffering2 = (char*)SaferResourceLoad(FMLFile);
-	//char* EndMySuffering2 = (char*)LockResource(LoadResource(NULL, FindResource(NULL, MAKEINTRESOURCE(FMLFile), RT_RCDATA)));
-	char* EndMySuffering = SaferMalloc(char, EndMySuffering, (size_t)CPCount * 4);
-	memcpy(EndMySuffering, EndMySuffering2, (size_t)CPCount * 4);
-	//Start printing the actual XML
-	FILE* GenericFile = fopen("F:\\My Programming Stuff Expansion\\ExcelFileTest\\xl\\worksheets\\sheet1.xml", "w+b");
-	(void)fprintf(GenericFile, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\" mc:Ignorable=\"x14ac\" xmlns:x14ac=\"http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac\"><dimension ref=\"%s1:%s%u\"/><sheetViews><sheetView tabSelected=\"1\" workbookViewId=\"0\"/></sheetViews><sheetFormatPr defaultRowHeight=\"15\" x14ac:dyDescent=\"0.25\"/><sheetData>", &EndMySuffering[MinCP * 4], &EndMySuffering[MaxCP * 4], MaxRow);
-	uint_fast32_t Row = 0;
-	uint_fast16_t TrueLevel[89] = { 10u, 15u, 20u, 25u, 30u, 35u, 40u, 45u, 50u, 55u, 60u, 65u, 70u, 75u, 80u, 85u, 90u, 95u, 100u, 105u, 110u, 115u, 120u, 125u, 130u, 135u, 140u, 145u, 150u, 155u, 160u, 165u, 170u, 175u, 180u, 185u, 190u, 195u, 200u, 205u, 210u, 215u, 220u, 225u, 230u, 235u, 240u, 245u, 250u, 255u, 260u, 265u, 270u, 275u, 280u, 285u, 290u, 295u, 300u, 305u, 310u, 315u, 320u, 325u, 330u, 335u, 340u, 345u, 350u, 355u, 360u, 365u, 370u, 375u, 380u, 385u, 390u, 395u, 400u, 405u, 410u, 415u, 420u, 425u, 430u, 435u, 440u, 445u, 450u };
-	//Rows with data in column 0 can store their data without
-	//r="A1" type tags until the first gap
-	//MaxRow = 2;
-	//while (Row < 2) {//While a row has data in column 0...
-	FastCPValue CPLeft = MinCP, CPRight = MaxCP;
-	if (MinCP == 0) {
-		CrappyPrintf("\nRow Parsing 1:\n%u\n", CPColumnHeight[0]);
-		do {//While a row has data in column 0...
-			CrappyPrintf("%u\r", Row);
-			(void)fprintf(GenericFile, "<row r=\"%u\" spans=\"1:%u\">", Row + 1, CPRight);
+	if (OutputMode == 0) {
+		char* EndMySuffering2 = (char*)SaferResourceLoad(FMLFile);
+		char* EndMySuffering = SaferMalloc(char, EndMySuffering, (size_t)CPCount * 4);
+		memcpy(EndMySuffering, EndMySuffering2, (size_t)CPCount * 4);
+		//Start printing the actual XML
+		//"F:\\My Programming Stuff Expansion\\ExcelFileTest\\xl\\worksheets\\sheet1.xml"
+		FILE* GenericFile = fopen(".\\sheet1.xml", "w+b");
+		(void)fprintf(GenericFile, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n<worksheet xmlns=\"http://schemas.openxmlformats.org/spreadsheetml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:mc=\"http://schemas.openxmlformats.org/markup-compatibility/2006\" mc:Ignorable=\"x14ac\" xmlns:x14ac=\"http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac\"><dimension ref=\"%s1:%s%u\"/><sheetViews><sheetView tabSelected=\"1\" workbookViewId=\"0\"/></sheetViews><sheetFormatPr defaultRowHeight=\"15\" x14ac:dyDescent=\"0.25\"/><sheetData>", &EndMySuffering[MinCP * 4], &EndMySuffering[MaxCP * 4], MaxRow);
+		uint_fast32_t Row = 0;
+		uint_fast16_t TrueLevel[89] = { 10u, 15u, 20u, 25u, 30u, 35u, 40u, 45u, 50u, 55u, 60u, 65u, 70u, 75u, 80u, 85u, 90u, 95u, 100u, 105u, 110u, 115u, 120u, 125u, 130u, 135u, 140u, 145u, 150u, 155u, 160u, 165u, 170u, 175u, 180u, 185u, 190u, 195u, 200u, 205u, 210u, 215u, 220u, 225u, 230u, 235u, 240u, 245u, 250u, 255u, 260u, 265u, 270u, 275u, 280u, 285u, 290u, 295u, 300u, 305u, 310u, 315u, 320u, 325u, 330u, 335u, 340u, 345u, 350u, 355u, 360u, 365u, 370u, 375u, 380u, 385u, 390u, 395u, 400u, 405u, 410u, 415u, 420u, 425u, 430u, 435u, 440u, 445u, 450u };
+		//Rows with data in column 0 can store their data without
+		//r="A1" type tags until the first gap
+		//MaxRow = 2;
+		//while (Row < 2) {//While a row has data in column 0...
+		FastCPValue CPLeft = MinCP, CPRight = MaxCP;
+		if (MinCP == 0) {
+			//CrappyPrintf("\nRow Parsing 1:\n%u\n", CPColumnHeight[0] - 1);
+			VerboseProgressHeader("\nRow Parsing 1:\n", CPColumnHeight[0] - 1);
+			do {//While a row has data in column 0...
+				//CrappyPrintf("%u\r", Row);
+				VerboseProgress(Row);
+				(void)fprintf(GenericFile, "<row r=\"%u\" spans=\"1:%u\">", Row + 1, CPRight);
+				while (Row > CPColumnHeight[CPRight]) {//Update the right bound of the data
+					--CPRight;
+				}
+				CP = CPLeft;
+				do {//...write cell data for that row...
+					(void)fprintf(GenericFile, "<c s=\"%u\"><v>%02u%02u%02u%02u</v></c>", CPColumn[CP][Row].Index, TrueLevel[CPColumn[CP][Row].Level], CPColumn[CP][Row].AttackIV, CPColumn[CP][Row].DefenseIV, CPColumn[CP][Row].HPIV);
+					++CP;
+				} while (Row < CPColumnHeight[CP]);//..until there's a gap in that data.
+				++CP;//There was just a gap, so it's safe to increment again
+				while (CP <= CPRight) {//While there's still data in the row...
+					if (Row < CPColumnHeight[CP]) {//...print cell data for the columns that aren't blank.
+						(void)fprintf(GenericFile, "<c r=\"%s%u\" s=\"%u\"><v>%02u%02u%02u%02u</v></c>", &EndMySuffering[CP * 4], Row + 1, CPColumn[CP][Row].Index, TrueLevel[CPColumn[CP][Row].Level], CPColumn[CP][Row].AttackIV, CPColumn[CP][Row].DefenseIV, CPColumn[CP][Row].HPIV);
+					}
+					++CP;
+				}
+				++Row;
+				(void)fputs("</row>", GenericFile);
+			} while (Row < CPColumnHeight[0]);//Ran out of rows with data in column 0, so now the left bound of each row is changing
+		}
+#ifdef PrintCrap
+		else {
+			//CrappyPrintf("\nSkipping Row Parsing 1...");
+			VerboseMessage("\nSkipping Row Parsing 1...");
+		}
+#endif
+		//CrappyPrintf("\nRow Parsing 2:\n%u\n", MaxRow - 1);
+		VerboseProgressHeader("\nRow Parsing 2:\n", MaxRow - 1);
+		while (Row < MaxRow) {//While there are still more rows...
+			//CrappyPrintf("%u\r", Row);
+			VerboseProgress(Row);
 			while (Row > CPColumnHeight[CPRight]) {//Update the right bound of the data
 				--CPRight;
 			}
+			while (Row > CPColumnHeight[CPLeft]) {//Update the left bound of the data
+				++CPLeft;
+			}
 			CP = CPLeft;
-			do {//...write cell data for that row...
-				(void)fprintf(GenericFile, "<c s=\"%u\"><v>%02u%02u%02u%02u</v></c>", CPColumn[CP][Row].Index, TrueLevel[CPColumn[CP][Row].Level], CPColumn[CP][Row].AttackIV, CPColumn[CP][Row].DefenseIV, CPColumn[CP][Row].HPIV);
-				++CP;
-			} while (Row < CPColumnHeight[CP]);//..until there's a gap in that data.
-			++CP;//There was just a gap, so it's safe to increment again
-			while (CP <= CPRight) {//While there's still data in the row...
+			(void)fprintf(GenericFile, "<row r=\"%u\" spans=\"%u:%u\">", Row + 1, CPLeft + 1, CPRight + 1);
+			do {//Starting from the left bound...
 				if (Row < CPColumnHeight[CP]) {//...print cell data for the columns that aren't blank.
 					(void)fprintf(GenericFile, "<c r=\"%s%u\" s=\"%u\"><v>%02u%02u%02u%02u</v></c>", &EndMySuffering[CP * 4], Row + 1, CPColumn[CP][Row].Index, TrueLevel[CPColumn[CP][Row].Level], CPColumn[CP][Row].AttackIV, CPColumn[CP][Row].DefenseIV, CPColumn[CP][Row].HPIV);
 				}
 				++CP;
-			}
+			} while (CP <= CPRight);
 			++Row;
 			(void)fputs("</row>", GenericFile);
-		} while (Row < CPColumnHeight[0]);//Ran out of rows with data in column 0, so now the left bound of each row is changing
+		};
+		//Print the XML footer
+		(void)fputs("</sheetData><pageMargins left=\"0.7\" right=\"0.7\" top=\"0.75\" bottom=\"0.75\" header=\"0.3\" footer=\"0.3\"/></worksheet>", GenericFile);
+		(void)fclose(GenericFile);
 	}
-#ifdef PrintCrap
 	else {
-		CrappyPrintf("\nSkipping Row Parsing 1...\n");
+		if (SpecificCP < MinCP || SpecificCP > MaxCP) {
+			printf("Specified CP is outside the calcualted range!");
+			exit(EXIT_FAILURE);
+		}
+		if (!CPColumnHeight[SpecificCP]) {
+			printf("No results for specified CP!");
+			exit(EXIT_FAILURE);
+		}
+		char* PokemonNames = (char*)SaferResourceLoad(PokemonNamesFile);
+		double TrueLevel[89] = { 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0, 10.5, 11.0, 11.5, 12.0, 12.5, 13.0, 13.5, 14.0, 14.5, 15.0, 15.5, 16.0, 16.5, 17.0, 17.5, 18.0, 18.5, 19.0, 19.5, 20.0, 20.5, 21.0, 21.5, 22.0, 22.5, 23.0, 23.5, 24.0, 24.5, 25.0, 25.5, 26.0, 26.5, 27.0, 27.5, 28.0, 28.5, 29.0, 29.5, 30.0, 30.5, 31.0, 31.5, 32.0, 32.5, 33.0, 33.5, 34.0, 34.5, 35.0, 35.5, 36.0, 36.5, 37.0, 37.5, 38.0, 38.5, 39.0, 39.5, 40.0, 40.5, 41.0, 41.5, 42.0, 42.5, 43.0, 43.5, 44.0, 44.5, 45.0 };
+		FILE* GenericFile = fopen(".\\CPOutput.txt", "w+b");
+		//CrappyPrintf("\nRow Parsing 1:\n%u\n", CPColumnHeight[SpecificCP] - 1);
+		VerboseProgressHeader("\nRow Parsing 1:\n", CPColumnHeight[SpecificCP] - 1);
+		for (uint_fast32_t Row = 0; Row < CPColumnHeight[SpecificCP]; ++Row) {
+			//CrappyPrintf("%u\r", Row);
+			VerboseProgress(Row);
+			(void)fprintf(GenericFile, "%23s/%4.1f/%02u/%02u/%02u\n", &PokemonNames[CPColumn[SpecificCP][Row].Index * 24], TrueLevel[CPColumn[SpecificCP][Row].Level], CPColumn[SpecificCP][Row].AttackIV, CPColumn[SpecificCP][Row].DefenseIV, CPColumn[SpecificCP][Row].HPIV);
+		}
+		//CrappyPrintf("\nSkipping Row Parsing 2...");
+		VerboseMessage("\nSkipping Row Parsing 2...");
+		(void)fclose(GenericFile);
 	}
-#endif
-	CrappyPrintf("\nRow Parsing 2:\n%u\n", MaxRow - 1);
-	while (Row < MaxRow) {//While there are still more rows...
-		CrappyPrintf("%u\r", Row);
-		while (Row > CPColumnHeight[CPRight]) {//Update the right bound of the data
-			--CPRight;
-		}
-		while (Row > CPColumnHeight[CPLeft]) {//Update the left bound of the data
-			++CPLeft;
-		}
-		CP = CPLeft;
-		(void)fprintf(GenericFile, "<row r=\"%u\" spans=\"%u:%u\">", Row + 1, CPLeft + 1, CPRight + 1);
-		do {//Starting from the left bound...
-			if (Row < CPColumnHeight[CP]) {//...print cell data for the columns that aren't blank.
-				(void)fprintf(GenericFile, "<c r=\"%s%u\" s=\"%u\"><v>%02u%02u%02u%02u</v></c>", &EndMySuffering[CP * 4], Row + 1, CPColumn[CP][Row].Index, TrueLevel[CPColumn[CP][Row].Level], CPColumn[CP][Row].AttackIV, CPColumn[CP][Row].DefenseIV, CPColumn[CP][Row].HPIV);
-			}
-			++CP;
-		} while (CP <= CPRight);
-		++Row;
-		(void)fputs("</row>", GenericFile);
-	};
-	//Print the XML footer
-	(void)fputs("</sheetData><pageMargins left=\"0.7\" right=\"0.7\" top=\"0.75\" bottom=\"0.75\" header=\"0.3\" footer=\"0.3\"/></worksheet>", GenericFile);
-	(void)fclose(GenericFile);
 	for (CP = 0; CP <= MaxCP; ++CP) {
 		free(CPColumn[CP]);
 	}
 	free(CPColumn);
-	printf("yeet");
-	CrappyPrintf("\nDone");
+	//CrappyPrintf("\nDone");
+	//VerboseMessage("\nDone");
+	VerboseMessage("\n");
+	printf("Done");
 	exit(EXIT_SUCCESS);
 }
